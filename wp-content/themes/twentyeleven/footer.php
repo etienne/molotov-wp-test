a8d7e2dd235f322364918c07073ea7d58c0c5cd6
Étienne Després <etienne.despres@molotovcoop.org>
2012-05-02 15:00:55 -0400

    Enlevé le crédit WordPress dans le footer.

--- wp-content/themes/twentyeleven/footer.php
+++ wp-content/themes/twentyeleven/footer.php
@@ -22,10 +22,6 @@
 					get_sidebar( 'footer' );
 			?>
 
-			<div id="site-generator">
-				<?php do_action( 'twentyeleven_credits' ); ?>
-				<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'twentyeleven' ) ); ?>" title="<?php esc_attr_e( 'Semantic Personal Publishing Platform', 'twentyeleven' ); ?>" rel="generator"><?php printf( __( 'Proudly powered by %s', 'twentyeleven' ), 'WordPress' ); ?></a>
-			</div>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
